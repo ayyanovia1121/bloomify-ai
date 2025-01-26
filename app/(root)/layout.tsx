3b5b438bf613873b9169f73054304055de2d3007
ayyanovia1121 <noviayya1121@gmail.com>
2025-01-26 12:12:22 +0700

    Shadcn Sheet Component for Mobile Navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 'use client'
+import MobileNavbar from "@/components/shared/MobileNavbar"
 import Sidebar from "@/components/shared/Sidebar"
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className='root'>
         <Sidebar />
+        <MobileNavbar />
         <div className="root-container">
             <div className="wrapper">{children}</div>
         </div>
