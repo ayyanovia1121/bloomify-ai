57a48618fa40fa8c046fd88ba13358b7082f8d23
ayyanovia1121 <noviayya1121@gmail.com>
2025-01-26 12:32:39 +0700

    Mobile Navbar DONE

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,9 @@
-import { UserButton,SignedIn, SignedOut, SignInButton} from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </div>
   );
 }
--- components/shared/MobileNavbar.tsx
+++ components/shared/MobileNavbar.tsx
@@ -1,9 +1,89 @@
-'use client'
+"use client";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const MobileNavbar = () => {
+    const pathname = usePathname();
   return (
-    <div>MobileNavbar</div>
-  )
-}
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          width={180}
+          height={28}
+        />
+      </Link>
 
-export default MobileNavbar
+      <nav className="flex items-center gap-2">
+        <SignedIn>
+          <UserButton />
+
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={152}
+                  height={32}
+                />
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
+                    return (
+                      <li
+                        key={link.route}
+                        className={`${
+                          isActive && "gradient-text"
+                        } p-18 flex whitespace-nowrap text-dark-700`}
+                      >
+                        <Link
+                          href={link.route}
+                          className="sidebar-link cursor-pointer"
+                        >
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  );
+};
+
+export default MobileNavbar;
