6ef7e3171837ead98fbbf41af0f5818493c7d174
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-15 16:01:30 +0700

    refactor(transformations): ✨ add properties and redirect logic in transformation type page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,30 @@
-import Header from "@/components/shared/Header";
-import { transformationTypes } from "@/constants";
 
-const AddTransformationTypePage = ({params :{type}} : SearchParamProps) => {
-    const transformation = transformationTypes[type];
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs/server";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId) return redirectToSignIn();
+
+  const user = await getUserById(userId);
   return (
-    <Header 
-    title={transformation.title}
-    subtitle={transformation.subTitle}/>
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user?.creditBalance}
+      />
+    </>
   );
 };
 
